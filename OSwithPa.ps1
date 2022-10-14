7beb122c6f090a2751e3d38b420b6703041a3f99
MyOLC <115734188+MyOLC@users.noreply.github.com>
2022-10-14 10:51:51 +0100

    Update OSwithPa.ps1

--- OSwithPa.ps1
+++ OSwithPa.ps1
@@ -2,12 +2,17 @@
 #   OS
 #   Start-OSDCloud with Params
 #================================================
+$Global:MyOSDCloud = @{
+	ApplyManufacturerDrivers = $false
+	ApplyCatalogDrivers = $false
+	ApplyCatalogFirmware = $false
+}
 $Params = @{
-    OSBuild = "21H2"
+    OSBuild = "Windows 10 21H2"
     OSEdition = "Enterprise"
     OSLanguage = "en-us"
     OSLicense = "Volume"
     SkipAutopilot = $true
     SkipODT = $true
 }
-Start-OSDCloud @Params
+Start-OSDCloud @Params-Firmware
