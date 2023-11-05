e31b915f674bfe5ff10296aa908613aa560b09c9
MyOLC <115734188+MyOLC@users.noreply.github.com>
2023-11-05 10:58:36 +0000

    Update OOBEManual.ps1

--- OOBEManual.ps1
+++ OOBEManual.ps1
@@ -1,2 +1,2 @@
 Start /Wait PowerShell -C Set-ExecutionPolicy Unrestricted; Install-Module OSD -Force ; Import-Module OSD -Force
-Start-OSDPad -RepoOwner MyOLC -RepoName MyScripts -BrandingTitle 'Our Learning Cloud - Bhavin Patel' -OAuth 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 -hide script -BrandingColor Magenta
+Start-OSDPad -RepoOwner MyOLC -RepoName MyScripts -BrandingTitle 'Our Learning Cloud - Bhavin Patel' -OAuth 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 -hide script -BrandingColor Magenta
