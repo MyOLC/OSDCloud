e8536b67b5dd4edf9dc0a3d8b352da26a787613a
MyOLC <115734188+MyOLC@users.noreply.github.com>
2023-11-05 07:20:00 +0000

    Create OOBEManual.ps1

---
+++ OOBEManual.ps1
@@ -0,0 +1,1 @@
+Start-OSDPad -RepoOwner MyOLC -RepoName MyScripts -BrandingTitle 'Our Learning Cloud - Bhavin Patel' -OAuth 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 -hide script -BrandingColor Magenta
