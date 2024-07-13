4b0b4230977c90dcb9a4c2e3e73c62c5e357103c
OurLearningCloud <115734188+MyOLC@users.noreply.github.com>
2024-07-13 21:25:42 +0100

    UpdateEdge Function

--- OOBEDeploy.ps1
+++ OOBEDeploy.ps1
@@ -19,6 +19,7 @@ $Global:oobeCloud = @{
     oobeRemoveAppxPackageName = 'Solitaire'
     oobeAddCapability = $false
     oobeAddCapabilityName = 'GroupPolicy','ServerManager','VolumeActivation'
+    oobeUpdateEdge = $true
     oobeUpdateDrivers = $true
     oobeUpdateWindows = $true
     oobeRestartComputer = $false
@@ -318,6 +319,60 @@ function Step-oobeStopComputer {
         Stop-Computer
     }
 }
+
+function Step-oobeUpdateEdge {
+    [CmdletBinding()]
+    param ()
+    if (($env:UserName -ne 'defaultuser0') -and ($Global:oobeCloud.oobeUpdateEdge -eq $true)) {    
+        $UpdateChannel = 'Stable'
+        $Architecture = 'x64'
+        $Platform = 'Windows'
+    switch ($UpdateChannel) {
+        'Stable' { $AppGUID = '{56EB18F8-B008-4CBD-B6D2-8C97FE7E9062}' }
+        'Beta' { $AppGUID = '{2CD8A007-E189-409D-A2C8-9AF4EF3C72AA}' }
+        'Canary' { $AppGUID = '{65C35B14-6C1D-4122-AC46-7148CC9D6497}' }
+        'Dev' { $AppGUID = '{0D50BFEC-CD6A-4F9A-964C-C7416E3ACB10}' }
+        }    
+        #Determine original Microsoft Edge Version
+        [System.Version]$EdgeVersionOld = (Get-AppxPackage -AllUsers -Name "Microsoft.MicrosoftEdge.$UpdateChannel").Version
+        if (!($EdgeVersionOld)) {
+            Write-Error "Microsoft Edge $UpdateChannel not installed, exiting"
+            $ExitCode = 1
+        }
+Else {
+    Write-Host "Current Microsoft Edge $UpdateChannel version $EdgeVersionOld"
+    #Determine latest Microsoft Edge Version depending on the update channel
+    $EdgeInfo = (Invoke-WebRequest -UseBasicParsing -uri 'https://edgeupdates.microsoft.com/api/products?view=enterprise')
+
+    [System.Version]$EdgeVersionLatest = ((($EdgeInfo.content | Convertfrom-json) | Where-Object {$_.product -eq $UpdateChannel}).releases | Where-Object {$_.Platform -eq $Platform -and $_.architecture -eq $architecture})[0].productversion
+    Write-Host "Latest $UpdateChannel Microsoft Edge version is $EdgeVersionLatest"
+				
+    #Check if Microsoft Edge is already up to date
+    If ($EdgeVersionOld -ge $EdgeVersionLatest) {
+        Write-Host "Microsoft Edge $UpdateChannel already up to date"
+        }
+    else {
+        #Trigger Microsoft Edge update
+        Write-Host "Launching Microsoft Edge $UpdateChannel update"
+        Start-Process -FilePath "C:\Program Files (x86)\Microsoft\EdgeUpdate\MicrosoftEdgeUpdate.exe" -argumentlist "/silent /install appguid=$AppGUID&appname=Microsoft%20Edge&needsadmin=True"
+        Write-Host "Sleeping for 60 seconds"
+        Start-Sleep -Seconds 60
+
+        #Getting new Microsoft Edge installed version
+        [System.Version]$EdgeVersionNew = (Get-AppxPackage -AllUsers -Name "Microsoft.MicrosoftEdge.$UpdateChannel").Version
+
+        # Do While Loop to wait until Microsoft Edge Version updated if required
+        Do {
+            [System.Version]$EdgeVersionNew = (Get-AppxPackage -AllUsers -Name "Microsoft.MicrosoftEdge.$UpdateChannel").Version
+            Write-Host "Checking current Edge version"
+            Start-Sleep -Seconds 15
+        } While ($EdgeVersionNew -lt $EdgeVersionLatest)
+        Write-Host "Microsoft Edge $UpdateChannel version updated to $EdgeVersionNew"
+    }
+
+    }
+    }
+}
 #endregion
 
 # Execute functions
@@ -332,6 +387,7 @@ Step-oobeRegisterAutopilot
 Step-EmbeddedProductKey
 Step-oobeRemoveAppxPackage
 Step-oobeAddCapability
+Step-oobeUpdateEdge
 Step-oobeUpdateDrivers
 Step-oobeUpdateWindows
 # Invoke-Webhook
